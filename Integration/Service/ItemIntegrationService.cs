513893572d321a5d0f3ca15a7f38dd0637e4a711
Fahrettin Enes <43903270+fahrettinenes@users.noreply.github.com>
2024-05-24 22:39:07 +0300

    refactor: remove redundant whitespaces

--- Integration/Service/ItemIntegrationService.cs
+++ Integration/Service/ItemIntegrationService.cs
@@ -12,14 +12,12 @@ public sealed class ItemIntegrationService
     //This is a dependency that is normally fulfilled externally.
     private ItemOperationBackend ItemIntegrationBackend { get; set; } = new();
 
-
     // This is called externally and can be called multithreaded, in parallel.
     // More than one item with the same content should not be saved. However,
     // calling this with different contents at the same time is OK, and should
     // be allowed for performance reasons.
     public Result SaveItem(string itemContent)
     {
-
         if (ItemIntegrationBackend.FindItemsWithContent(itemContent).Count != 0 || LocalCache.ContainsKey(itemContent))
         {
             return new Result(false, $"Duplicate item received with content {itemContent}.");
