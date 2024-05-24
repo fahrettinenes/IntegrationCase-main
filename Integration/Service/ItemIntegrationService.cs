19944f209865d910991a25352269d0f2de579171
Fahrettin Enes <43903270+fahrettinenes@users.noreply.github.com>
2024-05-24 22:39:35 +0300

    refactor: remove redundant whitespace

--- Integration/Service/ItemIntegrationService.cs
+++ Integration/Service/ItemIntegrationService.cs
@@ -11,7 +11,6 @@ public sealed class ItemIntegrationService
     //This is a dependency that is normally fulfilled externally.
     private ItemOperationBackend ItemIntegrationBackend { get; set; } = new();
 
-
     // This is called externally and can be called multithreaded, in parallel.
     // More than one item with the same content should not be saved. However,
     // calling this with different contents at the same time is OK, and should
